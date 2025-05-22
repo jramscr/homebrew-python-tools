9290c64aafb5dd913c14cc237561f55019431e13
Jesus Ramos <jramscr@gmail.com>
2025-05-21 22:33:18 -0600

    Update formula for v1.0.2

--- Formula/setup_virtualenv.rb
+++ Formula/setup_virtualenv.rb
@@ -1,8 +1,8 @@
 class SetupVirtualenv < Formula
   desc "Tool to setup and delete Python virtual environments easily"
-  homepage "https://github.com/YOUR_GITHUB_USERNAME/homebrew-python-tools"
-  url "https://github.com/jramscr/homebrew-python-tools/archive/refs/tags/v1.0.1.tar.gz"
-  version "1.0.1"
+  homepage "https://github.com/jramscr/homebrew-python-tools"
+  url "https://github.com/jramscr/homebrew-python-tools/archive/refs/tags/v1.0.2.tar.gz"
+  version "1.0.2"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   def install
