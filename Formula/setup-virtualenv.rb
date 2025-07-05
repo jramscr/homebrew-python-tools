a5b7e62e6aa161f2d1ca2370e77564b83e726316
Jesus Ramos <jramscr@Jesuss-Air.lan>
2025-07-05 11:39:37 -0600

    Update sha256 for v1.1.4

--- Formula/setup-virtualenv.rb
+++ Formula/setup-virtualenv.rb
@@ -3,7 +3,7 @@ class SetupVirtualenv < Formula
   homepage "https://github.com/jramscr/homebrew-python-tools"
   url "https://github.com/jramscr/homebrew-python-tools/archive/refs/tags/v1.1.3.tar.gz"
   version "1.1.4"
-  sha256 "04001ce12b461f0ba623cd2171c5962b477b77e15a8e84ef51eae4a197269d76"
+  sha256 "21d7b3a5f3320c7e083454a2046d4e0c5df9da1f22e3cf6de7847b596324077c"
 
   def install
     bin.install "bin/setup-virtualenv"
