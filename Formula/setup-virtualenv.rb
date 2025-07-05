571c30c042d36716b10558d718a4af2a0e646ef8
Jesus Ramos <jramscr@Jesuss-Air.lan>
2025-07-05 11:39:33 -0600

    Bump version to 1.1.4

--- Formula/setup-virtualenv.rb
+++ Formula/setup-virtualenv.rb
@@ -2,7 +2,7 @@ class SetupVirtualenv < Formula
   desc "Centralized Python virtual environment management with automatic configuration inference"
   homepage "https://github.com/jramscr/homebrew-python-tools"
   url "https://github.com/jramscr/homebrew-python-tools/archive/refs/tags/v1.1.3.tar.gz"
-  version "1.1.3"
+  version "1.1.4"
   sha256 "04001ce12b461f0ba623cd2171c5962b477b77e15a8e84ef51eae4a197269d76"
 
   def install
