a73581c0751116b66d1e2c5f79a5fd875f13b496
Jesus Ramos <jramscr@Jesuss-Air.lan>
2025-07-05 11:44:37 -0600

    Fix SHA256 for v1.1.3 (matches actual tarball)

--- Formula/setup-virtualenv.rb
+++ Formula/setup-virtualenv.rb
@@ -2,8 +2,8 @@ class SetupVirtualenv < Formula
   desc "Centralized Python virtual environment management with automatic configuration inference"
   homepage "https://github.com/jramscr/homebrew-python-tools"
   url "https://github.com/jramscr/homebrew-python-tools/archive/refs/tags/v1.1.3.tar.gz"
-  version "1.1.4"
-  sha256 "21d7b3a5f3320c7e083454a2046d4e0c5df9da1f22e3cf6de7847b596324077c"
+  version "1.1.3"
+  sha256 "04001ce12b461f0ba623cd2171c5962b477b77e15a8e84ef51eae4a197269d76"
 
   def install
     bin.install "bin/setup-virtualenv"
